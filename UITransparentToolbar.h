55b0cb56eb99375fb8df0601040412c223f348f8
Kristijan Sedlak <k.sedlak@mandatela.si>
2011-07-28 12:31:23 +0200

    iOS-UITransparentToolbar v1.0

---
+++ UITransparentToolbar.h
@@ -0,0 +1,16 @@
+//
+//  UITransparentToolbar.h
+//  iOS-UITransparentToolbar
+//
+//  Created by Kristijan Sedlak on 7/26/11 (originally by Dharshan Rangegowda).
+//  Copyright 2011 AppStrides. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UITransparentToolbar : UIToolbar 
+{    
+}
+
+@end
---
+++ UITransparentToolbar.m
@@ -0,0 +1,44 @@
+//
+//  UITransparentToolbar.m
+//  iOS-UITransparentToolbar
+//
+//  Created by Kristijan Sedlak on 7/26/11 (originally by Dharshan Rangegowda).
+//  Copyright 2011 AppStrides. All rights reserved.
+//
+
+#import "UITransparentToolbar.h"
+
+
+@implementation UITransparentToolbar
+
+// Override draw rect to avoid background coloring
+- (void)drawRect:(CGRect)rect 
+{
+    // do nothing in here
+}
+
+// Set properties to make background translucent.
+- (void) applyTranslucentBackground
+{
+    self.backgroundColor = [UIColor clearColor];
+    self.opaque = NO;
+    self.translucent = YES;
+}
+
+// Override init.
+- (id) init
+{
+    self = [super init];
+    [self applyTranslucentBackground];
+    return self;
+}
+
+// Override initWithFrame.
+- (id) initWithFrame:(CGRect) frame
+{
+    self = [super initWithFrame:frame];
+    [self applyTranslucentBackground];
+    return self;
+}
+
+@end
